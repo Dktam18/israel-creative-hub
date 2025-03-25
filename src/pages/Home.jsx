24f4be7fdcb31ce48a1afc2708fba1d10df98c48
Israel Mrakpor <diakportamenterprise@gmail.com>
2025-03-25 17:17:45 +0100

    Updated Home.jsx with Netlify changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -74,9 +74,9 @@ export const HomeContent = () => {
             <div className="right w-1/2 md:w-full relative">
               <div className="images relative heroImgblack">
                 <img
-                  src={heroImgback}
-                  alt=""
-                  className=" absolute top-32 left-10 w-96 md:left-10 hero"
+                  // src={heroImgback}
+                  // alt=""
+                  // className=" absolute top-32 left-10 w-96 md:left-10 hero"
                 />
                 <div className="heroImg h-[70vh] w-full hero-responsive ">
                   <img
@@ -87,39 +87,7 @@ export const HomeContent = () => {
                 </div>
               </div>
               <div className="content">
-                <button className="bg-white shadow-md absolute top-56 left-0 z-30 p-2 flex items-center rounded-md">
-                  {/* <div className="icon w-10 h-10 text-white rounded-full flex items-center justify-center bg-orange-400">
-                     <BsFillLightningChargeFill size={25} /> 
-                  </div> */}
-                  {/* <div className="text flex flex-col px-4">
-        
-                  </div> */}
-                </button>
-                <button className="bg-white shadow-md absolute bottom-32 left-48 z-30 p-2 flex items-center rounded-md pr-8">
-                  {/* <div className="icon w-10 h-10 text-white rounded-full flex items-center justify-center bg-blue-400">
-                    <FaGraduationCap size={25} />
-                  </div>
-                  <div className="text flex flex-col items-start px-4">
-                    <span className="text-sm text-black">10k</span>
-                    <span className="text-[12px]">reached students</span>
-                  </div> */}
-                </button>
-                <button className="bg-white shadow-md absolute top-56 -right-32 z-30 p-2  md:top-96 md:-right-5 flex items-center rounded-md">
-                  {/* <div className="icon w-10 h-10 text-white rounded-full flex items-center justify-center bg-orange-400">
-                    <FaUsers size={25} />
-                  </div>
-                  <div className="text flex flex-col items-start px-4">
-                    <span className="text-sm text-black">
-                      User Experience Class
-                    </span>
-                    <span className="text-[12px]">We learn every second</span>
-                  </div> */}
-                </button>
-                <button className="bg-white shadow-md absolute top-32 right-32 z-30 p-2 flex items-center rounded-md">
-                  {/* <div className="icon w-10 h-10 text-white rounded-full flex items-center justify-center bg-indigo-400">
-                    <FaBookReader size={25} />
-                  </div> */}
-                </button>
+               
               </div>
             </div>
           </div>
